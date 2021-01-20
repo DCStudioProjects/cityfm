d8fa66f489069494b48a361879c819544fc927c1
Daniil Chistyakov <4i.danila@gmail.com>
2021-01-20 09:40:29 +0300

    Bugs fixed; UI improved

--- src/Components/Lastsongs.js
+++ src/Components/Lastsongs.js
@@ -16,11 +16,11 @@ export default class Lastsongs extends Component {
     render() {
         return (
             <section className={style.last_songs}>
-                <h2>История эфира:</h2>
+                <h2 className={style.section_title}>История эфира:</h2>
                 <div className={style.song_history}>
                     {this.state.last?.map (song => (
                         <div className={style.last_song} key={song.id} style={{ backgroundImage: "url(" + song.artist.image + ")"}}>
-                            <p>{song.title} —&nbsp;</p>
+                            <p>{song.title}</p>
                             <a href={song.artist.url || song.artist.laut_url} target="_blank" rel="noreferrer"><p>{song.artist.name}</p></a>
                         </div>
                     ))}
