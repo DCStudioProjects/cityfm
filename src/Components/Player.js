1e5a394d623930b2cf08e98b35f5048306afd0c3
Daniil Chistyakov <4i.danila@gmail.com>
2021-01-24 21:11:45 +0300

    Cover bug fixed

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -13,7 +13,7 @@ export default class Player extends Component {
         const data = await response.json();
         this.setState({ data });
         console.log(data);
-        const images = await fetch("http://ws.audioscrobbler.com/2.0/?method=album.getInfo&artist=" + this.state.data?.artist.name + "&album=" + (this.state.data?.album || this.state.data?.title) + "&api_key=ac93b58817c64de67582b6350184ca24&format=json");
+        const images = await fetch("https://ws.audioscrobbler.com/2.0/?method=album.getInfo&artist=" + this.state.data?.artist.name + "&album=" + (this.state.data?.album || this.state.data?.title) + "&api_key=ac93b58817c64de67582b6350184ca24&format=json");
         const dataimage = await images.json();
         const noimage = "Загрузка...";
         this.setState({ dataimage });
