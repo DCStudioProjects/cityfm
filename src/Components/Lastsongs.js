87585e199811aa0e9df6f58d35a7f3b7b07b90af
Daniil Chistyakov <4i.danila@gmail.com>
2021-02-05 11:50:34 +0300

    UI improved

--- src/Components/Lastsongs.js
+++ src/Components/Lastsongs.js
@@ -11,10 +11,10 @@ export default class Lastsongs extends Component {
         const data1 = await response.json();
         const data2 = [];
         const array2 = [];
-        for (var i = 0; i < 5; i++) {
+        for (var i = 0; i < 7; i++) {
             let response = await fetch("https://ws.audioscrobbler.com/2.0/?method=album.getInfo&artist=" + data1[i].artist.name.replace(' & ', ', ') + "&album=" + data1[i].album + "&api_key=ac93b58817c64de67582b6350184ca24&format=json");
             data2[i] = await response.json();
-            let array1 = {id: data1[i].id, artist: data1[i].artist.name, title: data1[i].title, cover: data2[i].album.image[4]["#text"]}
+            let array1 = {id1: data1[i].id, id2: data1[i].id + 1, artist: data1[i].artist.name, title: data1[i].title, cover: data2[i].album.image[4]["#text"]}
             array2[i] = array1
         }
         this.setState({results: array2})
@@ -28,13 +28,12 @@ export default class Lastsongs extends Component {
                 <h2 className={style.section_title}>История эфира:</h2>
                 <div className={style.song_history}>
                     {this.state.results?.map (song => (
-                        <div className={style.mobile}>
-                        <div className={style.last_song} key={song.id} style={{ backgroundImage: "url(" + song.cover + ")"}}>     
-                        <p className={style.name_desktop} key={song.id}>{song.artist} — {song.title}</p>
+                        <div className={style.mobile} key={song.id1}>
+                        <div className={style.last_song} key={song.id1} style={{ backgroundImage: "url(" + song.cover + ")"}}>     
                         </div>
                         <div>
-                        <p className={style.name_mobile} key={song.id}>{song.title}</p>
-                        <p className={style.name_mobile} key={song.id}>{song.artist}</p>
+                        <p className={style.song_meta} key={song.id1}>{song.title}</p>
+                        <p className={style.song_meta} key={song.id2}>{song.artist}</p>
                         </div>
                         </div>
                     ))}
