dc7b4d5d2a1ccbf123568cfcac037218723ddfc7
Daniil Chistyakov <4i.danila@gmail.com>
2021-02-07 21:58:00 +0300

    Song time added into Lastsongs section

--- src/Pages/Header.js
+++ src/Pages/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Home from './Home';
+import Test from './Test';
 import err404 from './404';
 
 export default class Header extends Component {
@@ -14,6 +15,8 @@ export default class Header extends Component {
                 </header>
                 <Switch>
                 <Route exact path="/" component={Home}></Route>
+                <Route exact path="/test" component={Test}></Route>
+
                 <Route path="*" component={err404} />
             </Switch>
             </Router>
