c35cbdfda3d5c24319fb018d63d1191e6d6fe6cf
Daniil Chistyakov <4i.danila@gmail.com>
2021-03-01 23:43:25 +0300

    Bugs fixed

--- src/Components/Chart.js
+++ src/Components/Chart.js
@@ -48,7 +48,7 @@ export default class Chart extends Component {
                             </div>
                         ))}
                     </div>
-                    <div className={style.lastsongs_next}><Link to="/lastsongs">Далее</Link></div>
+                    <div className={style.lastsongs_next}><Link to="/chart">Далее</Link></div>
                 </div>
             </section>
         )
--- src/Pages/Cam.js
+++ src/Pages/Cam.js
@@ -5,7 +5,7 @@ export default class Cam extends Component {
         return (
             <div>
                 <h1>Вебкамера</h1>
-                <iframe src="https://player.twitch.tv/?channel=danielchistyakov&parent=www.cityfm.gq" title="Вебкамера" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="100%"></iframe>
+                <iframe src="https://player.twitch.tv/?channel=danielchistyakov&parent=www.yandex.ru" title="Вебкамера" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="100%"></iframe>
                 <p>Не грузит трансляцию? DJ пока не может показать себя, попробуйте чуть позже :(</p>
             </div>
         )
