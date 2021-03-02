f8496770b0451cb55b2914c003166dcd38c376e3
Daniil Chistyakov <4i.danila@gmail.com>
2021-03-02 20:24:58 +0300

    Cam fatal bug fixed Chat fatal bug fixed

--- src/Pages/Cam.js
+++ src/Pages/Cam.js
@@ -5,7 +5,7 @@ export default class Cam extends Component {
         return (
             <div>
                 <h1>Вебкамера</h1>
-                <iframe src="https://player.twitch.tv/?channel=danielchistyakov&parent=www.yandex.ru" title="Вебкамера" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="100%"></iframe>
+                <iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2F100029826813324%2Fvideos%2F482320996105457%2F&show_text=false&width=560" width="560" height="314" style={{ border: 'none', overflow: 'hidden' }} scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
                 <p>Не грузит трансляцию? DJ пока не может показать себя, попробуйте чуть позже :(</p>
             </div>
         )
--- src/Pages/Chat.js
+++ src/Pages/Chat.js
@@ -5,7 +5,17 @@ export default class Chat extends Component {
   render() {
     return (
       <div>
-
+        <div id="sigCommentsBlock"></div>
+        <script type="text/javascript">
+          {(function () {
+            var host_id = '5096';
+            var script = document.createElement('script');
+            script.type = 'text/javascript';
+            script.async = true;
+            script.src = '//sigcomments.com/chat/?host_id=' + host_id;
+            var ss = document.getElementsByTagName('script')[0];
+            ss.parentNode.insertBefore(script, ss);
+          })()};</script>
       </div>
     )
   }
